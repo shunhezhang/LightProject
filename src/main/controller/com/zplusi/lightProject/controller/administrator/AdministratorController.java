3ae16ee490df05504bd63a066a8b5e2b01b11668
shunhezhang <usherz@163.com>
2017-10-12 14:39:15 +0800

    Update Test
    Update Test

--- src/main/controller/com/zplusi/lightProject/controller/administrator/AdministratorController.java
+++ src/main/controller/com/zplusi/lightProject/controller/administrator/AdministratorController.java
@@ -14,7 +14,6 @@ import com.zplusi.lightProject.service.administrator.AdministratorService;
 /**
  * =======================<br/>
  * 管理员管理控制类，处理管理员管理的前后端交互
- * test commit again
  * Date：2016-12-25 22:31:10
  * Author：shunhezhang
  * =======================<br/>
